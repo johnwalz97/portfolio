f388f803f9b73a29b741a0400169fed69e74513c
johnwalz97 <johnwalz97@gmail.com>
2015-12-29 13:11:27 -0500

    initial commit`

---
+++ server.js
@@ -0,0 +1,21 @@
+//Setup modules
+var flag = false;
+var express = require("express");
+var path = require("path");
+var app = express();
+var bodyParser = require("body-parser");
+
+//Setup app
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, "./")));
+app.set('views', path.join(__dirname, './'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.json(urls2);
+})
+
+//Server
+app.listen(80, function() {
+    console.log("listening on port 80");
+})
